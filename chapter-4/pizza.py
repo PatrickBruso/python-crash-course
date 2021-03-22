f7a9bdb288893442ffa553075ca0a8cd5ae71f76
Patrick Bruso <pkbruso@gmail.com>
2021-03-21 21:49:09 -0700

    added pizza.py

---
+++ chapter-4/pizza.py
@@ -0,0 +1,8 @@
+pizzas = ['margherita', 'pepperoni', 'hawaiian']
+
+for pizza in pizzas:
+    print(pizza.title())
+
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza.")
+print("I really love pizza!")
