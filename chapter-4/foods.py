26bce93aea77b7d289deb303ea301b72fe74fd02
Patrick Bruso <pkbruso@gmail.com>
2021-03-22 17:25:22 -0700

    added program about copying lists

---
+++ chapter-4/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:] # slice entire list into new list
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorit foods are:")
+print(friend_foods)
