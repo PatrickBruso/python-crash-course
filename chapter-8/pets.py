e41e177ee0ec7bd9bae51e12f36e2cfdb6f9f07e
Patrick Bruso <pkbruso@gmail.com>
2022-05-12 13:48:50 -0700

    Adding more programs for chapter 7

---
+++ chapter-8/pets.py
@@ -0,0 +1,9 @@
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
+
+# Using keyword arguments
+describe_pet(animal_type='dog', pet_name='roger')
