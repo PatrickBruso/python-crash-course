bb09fc1e1bfe9f8c5407624e52b5f0073aa3de1c
Patrick Bruso <pkbruso@gmail.com>
2022-05-13 21:26:08 -0700

    Added programs for chapter 9

--- chapter-9/car.py
+++ chapter-9/car.py
@@ -16,7 +16,32 @@ class Car:
     def read_odometer(self):
         """Print a statement showing the car's mileage"""
         print(f"This car has {self.odometer_reading} miles on it.")
+    
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attemps to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descrive_name())
+my_new_car.read_odometer()
+
+# Modify attribute value directly
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
+# Modify attribute value using method
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_new_car.increment_odometer(100)
 my_new_car.read_odometer()
