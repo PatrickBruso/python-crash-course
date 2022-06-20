77c9f7896172cc3866735b352b8ef16f809ed2b5
Patrick Bruso <pkbruso@gmail.com>
2022-06-20 16:36:52 -0700

    Added newline character

--- chapter-10/write_message.py
+++ chapter-10/write_message.py
@@ -1,6 +1,6 @@
 # Simple example of writing a file 
 
-filename = 'programming.txt'
+filename = 'chapter-10/programming.txt'
 
 with open(filename, 'w') as file_object:
     file_object.write("I love programming.\n") # Add mewline character to put next write on new line.
