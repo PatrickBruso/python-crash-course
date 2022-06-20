5582dc9dde1bccee62f8147ca48b683752fa995b
Patrick Bruso <pkbruso@gmail.com>
2022-06-20 16:34:32 -0700

    Added program to write to file

---
+++ chapter-10/write_message.py
@@ -0,0 +1,7 @@
+# Simple example of writing a file 
+
+filename = 'programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.\n") # Add mewline character to put next write on new line.
+    file_object.write("I love creating new games.\n")
